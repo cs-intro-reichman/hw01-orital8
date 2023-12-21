06750abb1d731d6ff24afc50f3bc3b7d6baa6bb0
orital8 <154499909+orital8@users.noreply.github.com>
2023-12-21 09:36:48 +0200

    Update AddTwo.java
    update

--- AddTwo.java
+++ AddTwo.java
@@ -6,10 +6,7 @@ public class AddTwo {
 	public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);	
-		
-		System.out.println(" java AddTwo "+ a +" "+ b);
-		System.out.println(a + "+" + b + "=" + (a + b));
-		
+		System.out.println(a + "+" + b + "=" + (a + b))	;
 	}
 }
  
