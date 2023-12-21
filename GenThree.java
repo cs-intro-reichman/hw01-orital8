e4f5f47ecada1ff901d175de32438118a7c3ce4e
orital8 <154499909+orital8@users.noreply.github.com>
2023-12-21 20:33:42 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,30 +4,19 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here
-        // enter 2 numbers 
-            int a = Integer.parseInt(args[0]);
-            int b = Integer.parseInt(args[1]);
-            //for finding the range between the upcoming random numbers i need to find which one is bigger
-            int min = Math.min(a,b);
-            int max = Math.max(a,b); 
-            //range is important for finding the multiply number
-            int range= max-min;
-            int num1= (int)(Math.random()*range) + min;
-            int num2= (int)(Math.random()*range) + min;
-            int num3= (int)(Math.random()*range) + min;
-            //min between first 2  random numbers
-            int minimal= Math.min(num1,num2);
-            System.out.println(num1);
-            System.out.println(num2);
-            System.out.println(num3);
-            System.out.println("The minimal generated number was "+ Math.min(minimal, num3));
-
-                   }
-        }
-
-    
-    
-	
-	
-
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		int mx= Math.max(a,b);
+		int mn= Math.min(a,b);
+		int randomNumber1 = (int) (Math.random() * (mx - mn + 1)) + mn;
+		int randomNumber2 = (int) (Math.random() * (mx - mn + 1)) + mn;
+		int randomNumber3 = (int) (Math.random() * (mx - mn + 1)) + mn;
+		
+		int random_min= Math.min( Math.min(randomNumber1,randomNumber2),randomNumber3);	
+		
+			System.out.println((randomNumber1)+"\n"+(randomNumber2)+"\n"+(randomNumber3)+"\n"+
+													("The minimal generated number was "+random_min));
+		
+	}
+}
