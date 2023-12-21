4105a3fb60cc7dfcb6fff16aa7d11ca69dcf4541
orital8 <154499909+orital8@users.noreply.github.com>
2023-12-21 17:23:49 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,16 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		if
+		((a+b)>c && (b+c)>a && (c+a)>b)
+			{
+				System.out.println( a +",  "+ b+",  "+c+": true ");
+			}	
+			else 
+				System.out.println( a +",  "+ b+",  "+c+": false ");
+			
 	}
 }
