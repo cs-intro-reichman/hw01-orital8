b4e4655b5990d738e554e8f99ca346438cfb6be4
orital8 <154499909+orital8@users.noreply.github.com>
2023-12-25 11:12:26 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -15,8 +15,8 @@ public class GenThree {
 		
 		int random_min= Math.min( Math.min(randomNumber1,randomNumber2),randomNumber3);	
 		
-			System.out.println((randomNumber1)+"\n"+(randomNumber2)+"\n"+(randomNumber3)+"\n"+
-													("The minimal generated number was "+random_min));
+		System.out.println(randomNumber1+"\n"+randomNumber2+"\n"+randomNumber3+"\n"));
+		System.out.println("The minimal generated number was "+random_min);
 		
 	}
 }
